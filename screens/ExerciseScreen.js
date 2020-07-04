420f00873cae226d7ca4ac5a4839c32432474690
shiv raj <shiv1988@gmail.com>
2020-07-04 18:52:44 -0400

    Removed table in exercise screen

--- screens/ExerciseScreen.js
+++ screens/ExerciseScreen.js
@@ -1,25 +1,21 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { StyleSheet, Text, View, ImageBackground, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, ScrollView, SafeAreaView, FlatList } from 'react-native';
 import { expandExercise, maxWeight } from '../store/actions/actions';
-import { Table, Row, Rows } from 'react-native-table-component';
 import WorkoutInput from '../components/WorkoutInput';
 import BackImg from '../assets/back-workout.jpg';
+import { SimpleLineIcons, Octicons, FontAwesome } from '@expo/vector-icons';
+import Colors from '../constants/colors';
 
 const ExerciseScreen = props => {
     const selectedExercise = props.route.params ? props.route.params.exercise : null;
-    //const selectedExercise = props.navigation.getParam('exercise');
     const logs = useSelector(state => state.fitlogReducer.logs);
     const workouts = useSelector(state => state.fitlogReducer.workouts);
     const maxWt = useSelector(state => state.fitlogReducer.maxWeight);
     const user = useSelector(state => state.fitlogReducer.user);
     const category = props.route.params ? props.route.params.workout : null;
-    //const category = props.navigation.getParam('workout');
     const userId = '5dfecbdd39d8760019968d04';
     const dispatch = useDispatch();
-
-    const tableHead = ['Date & Time', 'Weight', 'Unit', 'Count'];
-    const tableData = logs.map(exercise => [exercise.date, exercise.weight, exercise.unit, exercise.count]);
 
     useEffect(() => {
         dispatch(expandExercise(workouts, category, selectedExercise, userId));
@@ -38,18 +34,22 @@ const ExerciseScreen = props => {
                         <Text style={styles.maxwtText}>Count: </Text>
                         <Text>{maxWt.count}</Text>
                     </Text>}
-                    {tableData && tableData.length > 0 ?
-                        <React.Fragment>
-                            <Table borderStyle={{ borderWidth: 1, borderColor: 'transparent' }}>
-                                <Row data={tableHead} style={styles.head} flexArr={[3, 1, 1, 1]} textStyle={styles.headText} />
-                            </Table>
-                            <ScrollView>
-                                <Table borderStyle={{ borderWidth: 1, borderColor: 'transparent' }}>
-                                    <Rows data={tableData} textStyle={styles.text} style={styles.row} flexArr={[3, 1, 1, 1]} />
-                                </Table>
-                            </ScrollView>
-                        </React.Fragment> : 
-                        <Text style={styles.start}>Start Logging!</Text>}
+                    <SafeAreaView style={{ flex: 1 }}>
+                        <View>
+                            <FlatList
+                                data={logs}
+                                renderItem={({ item }) =>
+                                    <View style={styles.logs}>
+                                        <View style={{ flex: 1 }}><SimpleLineIcons name="note" size={24} color="black" /></View>
+                                        <View style={{ flex: 1 }}><FontAwesome name="trophy" size={25} color={Colors.buttonColor} /></View>
+                                        <View style={{ flex: 2 }}><Text>{item.date}</Text></View>
+                                        <View style={{ flex: 1 }}><Text style={{ textAlign: 'right' }}>{item.weight} {item.unit}</Text></View>
+                                        <View style={{ flex: 1 }}><Text style={{ textAlign: 'right' }}>{item.count} reps</Text></View>
+                                    </View>}
+                                keyExtractor={item => item.date}
+                            />
+                        </View>
+                    </SafeAreaView>
                 </View>
             </ImageBackground>
         </View>
@@ -57,7 +57,6 @@ const ExerciseScreen = props => {
 }
 
 export const screenOptions = navigationData => {
-    console.log('Navigation data: ', navigationData);
     return {
         headerTitle: navigationData.route.params.params.exercise
     }
@@ -74,27 +73,31 @@ const styles = StyleSheet.create({
         backgroundColor: 'rgba(238, 238, 238, 0.8)',
         height: '100%'
     },
-    header: {
-        paddingHorizontal: 10
+    image: { flex: 1 },
+    overlay: { backgroundColor: 'rgba(255,0,0,0.5)' },
+    maxwt: { 
+        height: 40, 
+        textAlign: 'center' 
     },
-    image: {
-        flex: 1
-    },
-    overlay: {
-        backgroundColor: 'rgba(255,0,0,0.5)',
-    },
-    maxwt: { height: 40, textAlign: 'center' },
     maxwtText: { fontWeight: 'bold' },
-    head: { height: 40, backgroundColor: 'darkgrey' },
-    headText: { margin: 6, textAlign: 'center', fontWeight: 'bold' },
-    text: { margin: 6, textAlign: 'center' },
-    row: { height: 40 },
+    text: { 
+        margin: 6, 
+        textAlign: 'center' 
+    },
     start: {
         flex: 1,
         textAlign: 'center',
         marginTop: 90,
         fontWeight: 'bold'
     },
+    logs: {
+        paddingVertical: 8,
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        borderBottomWidth: 1,
+        borderBottomColor: 'darkgrey'
+    }
 });
 
 export default ExerciseScreen;
